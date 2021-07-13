8bf2a675d8bade712668fb67d21b3fb3cc0f3d45
Muh <mmtariqk@gmail.com>
2021-07-13 12:40:05 -0700

    updates

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -34,10 +34,10 @@ const Success = () => {
       <Jumbotron>
         <h1>Success!</h1>
         <h2>
-          Thank you for your purchase!
+          Thank you so much for your purchase!
         </h2>
         <h2>
-          You will now be redirected to the home page
+          You will now be redirected to the homepage
         </h2>
       </Jumbotron>
     </div>
