a88a00266a2c42dec264394bb93afe83e3e8f11b
Muh <mmtariqk@gmail.com>
2021-07-13 11:47:43 -0700

    clean up

--- server/seed/seeds.js
+++ server/seed/seeds.js
@@ -110,7 +110,7 @@ db.once('open', async () => {
       name: 'Teddy Bear',
       category: categories[4]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
       image: 'teddy-bear.jpg',
       price: 7.99,
       quantity: 100
